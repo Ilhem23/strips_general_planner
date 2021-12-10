f0b0461fd0fdcfeae0dc8e5d3c1586b24d44cde4
Ilhem23 <ei_aissaoui@esi.dz>
2021-12-10 20:21:16 +0100

    add general parse for the initial

--- strips.py
+++ strips.py
@@ -1,6 +1,17 @@
 import sys
 
 
+# general function to parse return the name and argument in tuple
+def general_parser(line):
+    name = line[:line.find('(')]
+    rest_of_the_line = line[line.find(')') + 1:]
+    rest_of_the_line = rest_of_the_line.strip(', ')
+    line = line[line.find('(') + 1:line.find(')')]
+    args = line.split(',')
+    args = [a.strip(' ') for a in args]
+    return name, args, rest_of_the_line
+
+
 # function of initial state parse
 def parser(file):
     # read the first line in the file
@@ -13,10 +24,13 @@ def parser(file):
     if words[0] == "Initial" and words[1] == "state:":
         # retrieve the whole line without the Initial state words and put it in list
         words = words[2:]
-        # loop over the previous line
-        for i in words:
-            # add to the list each state as tuple
-            init_state.append((i[:i.find('(')], i[(i.find('(') + 1):(i.find(')'))]))
+        # convert to string
+        new_line = ' '.join(words)
+        # loop over the new string and extract the name and the argument, delete the extracted one and return
+        # the new line without the extracted one
+        while len(new_line):
+            name, args, new_line = general_parser(new_line)
+            init_state.append((name, args))
     return init_state
 
 
@@ -24,7 +38,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: ./strips FILE.TXT")
         exit(1)
-# read the file insert in command line
+    # read the file insert in command line
     with open(sys.argv[1]) as f:
         print(parser(f))
     exit(0)
